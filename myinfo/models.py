daf672fc41e16cbe6297cb4b2c296c5a980fa1ea
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2025-02-24 16:12:09 +0700

    feat: add OauthSessionState model and its migration file

--- myinfo/models.py
+++ myinfo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.sessions.models import Session
 
-# Create your models here.
+class OauthSessionState(models.Model):
+    session = models.OneToOneField(Session, on_delete=models.CASCADE, primary_key=True)
+    key = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
