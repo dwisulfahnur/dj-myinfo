8e539a5a452dc8ed222ef3d1fcc32ebec244c74e
Dwi Sulfahnur <dwisulfahnur@gmail.com>
2025-02-25 01:20:47 +0700

    feat: add new views (index, callback)

--- myinfo/views.py
+++ myinfo/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponseNotFound
+from django.http.request import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+def index_view(request: HttpRequest):
+    return render(request, 'myinfo/index.html')
+
+def callback_form_view(request: HttpRequest):
+    code = request.GET.get('code')
+    if not code:
+        return HttpResponseNotFound("Not Found")
+    return render(request, 'myinfo/form.html')
